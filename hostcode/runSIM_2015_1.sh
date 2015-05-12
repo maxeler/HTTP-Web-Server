e751dcf5f37dcd85a18428f63879184059a0c707
Milan Vorkapic <mvorkapic@maxeler.com>
2015-05-12 15:40:05 +0100

    updated file names

---
+++ hostcode/runSIM_2015_1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#source "/network-raid/opt/maxcompiler-2014.2/settings.sh"
+source "/network-raid/opt/maxcompiler-2015.1-post-ikon-merge/settings.sh"
+
+export MAXELEROSDIR=$MAXCOMPILERDIR/lib/maxeleros-sim
+export LD_PRELOAD=$MAXELEROSDIR/lib/libmaxeleros.so:$LD_PRELOAD
+export SLIC_CONF="$SLIC_CONF;use_simulation=${USER}Sim"
+
+
+
+# How to set a BASH variable equal to the output from a command?
+# You need `, not '. This character is called "backticks" 
+srcMax=`maxGuessBuildDir -s httpServer_ISCA_DFE_SIM`results/httpServer.max
+srcH=`maxGuessBuildDir -s httpServer_ISCA_DFE_SIM`results/httpServer.h
+echo "Copying .max and .h files to the /hostname directory"
+echo $srcMax
+echo $srcH
+cp $srcMax /home/${USER}/workspace/workingNonMaxIDE/hostcode
+cp $srcH /home/${USER}/workspace/workingNonMaxIDE/hostcode
+echo "Finished copying"
+
+# 1) run 'make_initCode.py' (python fabricate)
+python make_initCode.py
+# 2) run 'make_httpServerCPUcode.py' (python fabricate) 
+python make_httpServerCPUcode.py
