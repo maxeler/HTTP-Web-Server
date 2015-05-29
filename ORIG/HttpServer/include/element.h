5edac68b5af319cd410dc620699e00f4ff463724
Milan Vorkapic <mvorkapic@maxeler.com>
2015-05-29 12:47:48 +0100

    added element.h #27

---
+++ ORIG/HttpServer/include/element.h
@@ -0,0 +1,10 @@
+#ifndef ELEMENT_H_INCLUDED
+#define ELEMENT_H_INCLUDED
+
+struct Element {
+    char* pointer;
+    int sizeBytes;
+};
+
+#endif  // ELEMENT_H_INCLUDED
+
--- ORIG/HttpServer/include/init.h
+++ ORIG/HttpServer/include/init.h
@@ -1,10 +1,7 @@
 #ifndef INIT_H_INCLUDED
 #define INIT_H_INCLUDED
 
-struct Element {
-    char* pointer;
-    int sizeBytes;
-};
+#include "element.h"
 
 unsigned int calculateCRC16(unsigned char *Data, unsigned char len);
 int initCode(struct Element *crcTable, char* cdir);
